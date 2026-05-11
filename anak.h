2f4f2680a3a447b0a6d4499f602a02ba48dcc355
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-11 19:42:25 +0700

    first commit

---
+++ anak.h
@@ -0,0 +1,19 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+public:
+    //deklarasi member variabel pada class anak
+    string nama;
+
+    //membuat constructor dan destructor pada class anak
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \ "" << nama << "\" ada \n";
+    }
+
+    ~anak() {
+        cout << "Anak \ "" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif
