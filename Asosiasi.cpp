18bd3d1f170b7bb955f1bd9c139d0c27c41841a3
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-11 20:08:22 +0700

    deklarasi prosedur tambah pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -42,5 +42,9 @@ public:
         cout << "Dokter \"" << nama << "\" tidak ada\n";
     }
 
+//deklarasi prosedur tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
 
 };
