92c662c1a9b4ca4a4015a381d1c971064cbbda47
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-11 20:03:33 +0700

    buat class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//deklarasi class dokter
+class dokter;
+
+//deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    //deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+};
