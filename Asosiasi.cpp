350b8dc1779d8f86b9390c165cdb3b470e813909
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-11 20:09:06 +0700

    buat method pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -46,5 +46,19 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 
-
 };
+
+//pendefinisian prosedur - prosedur dari class dokter dan class pasien diluar class
+void pasien::tambahDoker(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+};
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
