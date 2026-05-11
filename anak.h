47cd9cfdaf60feec55079633c8a1a98d8c80e568
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-11 19:49:26 +0700

    tambah constructor anak

--- anak.h
+++ anak.h
@@ -5,6 +5,11 @@ class anak {
 public:
     //deklarasi member variabel pada class anak
     string nama;
+     //membuat constructor dan destructor pada class anak
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \ "" << nama << "\" ada \n";
+    }
+
 
 };
 
