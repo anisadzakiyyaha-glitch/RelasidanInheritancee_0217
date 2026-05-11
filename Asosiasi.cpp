d9282edf46d32c3bf5228c432da53711937f364b
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-11 20:06:54 +0700

    pembuatan class dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -25,3 +25,12 @@ public:
     void tambahDoker(dokter*);
     void cetakDokter();
 };
+
+//pembuatan class dokter
+class dokter {
+public:
+    //deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+};
